f802aa8e77fd1e296cad3c207b4976b617dbac71
JunKakizako <111491185+kakizakojun@users.noreply.github.com>
2022-09-14 14:39:11 +0000

    [add]bootstrap途中

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -10,7 +10,6 @@ class PostCommentsController < ApplicationController
   def destroy
     PostComment.find(params[:id]).destroy
     redirect_to post_image_path(params[:post_image_id])
-    redirect_to post_image_path(params[:post_image_id])
   end
   
   private
--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,12 +6,15 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
+    if @post_image.save
     redirect_to post_images_path
+    else 
+      render :new
+    end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
@@ -22,7 +25,7 @@ class PostImagesController < ApplicationController
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
-    redirect_to '/post_images'
+    redirect_to post_images_path
   end
 
   private
